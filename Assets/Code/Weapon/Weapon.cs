8895081a1efc56597ad8d6f3c79cbbc1e0285261
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-06-03 23:41:40 +0300

    add: animation idile

--- Assets/Code/Weapon/Weapon.cs
+++ Assets/Code/Weapon/Weapon.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Code.Weapon
@@ -7,14 +8,41 @@ namespace Code.Weapon
         [SerializeField] private ParticleSystem _shootEffect;
         [SerializeField] private GameObject _pivot;
         [SerializeField] private GameObject _model;
-        
+
+        [SerializeField] private float _amplitude = 0.3f; // вверх/вниз
+        [SerializeField] private float _duration = 0.5f;
+        [SerializeField] private Ease _ease = Ease.InOutSine;
+
+        private Tween _tween;
+        private Vector3 _baseLocalPos;
+
+        private void Awake()
+        {
+            _baseLocalPos = _pivot.transform.localPosition;
+        }
+
         public GameObject Pivot => _pivot;
-        
         public GameObject Model => _model;
-        
+
         public void PlayShootEffect()
         {
             _shootEffect.Play();
+        }
+
+        public void Play()
+        {
+            Stop();
+
+            _tween = DOTween.Sequence()
+                .Append(_pivot.transform.DOLocalMoveY(_baseLocalPos.y + _amplitude, _duration).SetEase(_ease))
+                .Append(_pivot.transform.DOLocalMoveY(_baseLocalPos.y - _amplitude, _duration).SetEase(_ease))
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+
+        public void Stop()
+        {
+            _tween?.Kill();
+            _tween = _pivot.transform.DOLocalMoveY(_baseLocalPos.y, 0.3f).SetEase(Ease.InOutSine);
         }
     }
 }
