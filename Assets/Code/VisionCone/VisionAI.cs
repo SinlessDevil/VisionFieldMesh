de47c70c7b5b4aaf0dbc5427bd05af41e21fdd1d
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-06-03 19:00:51 +0300

    add: base weapon ai

--- Assets/Code/VisionCone/VisionAI.cs
+++ Assets/Code/VisionCone/VisionAI.cs
@@ -1,10 +1,13 @@
 using Code.VisionCone.Detectors;
+using Code.Weapon;
 using UnityEngine;
 
 namespace Code.VisionCone
 {
     public class VisionAI : MonoBehaviour
     {
+        [SerializeField] private WeaponAI _weaponAI;
+        
         private BaseTargetDetector _detector;
 
         public void Initialize(BaseTargetDetector detector)
@@ -26,11 +29,15 @@ namespace Code.VisionCone
         private void HandleTargetDetected(Enemy enemy)
         {
             Debug.Log($"Enemy detected {enemy.name}");
+            
+            _weaponAI.StartFiring(enemy);
         }
 
         private void HandleTargetLost(Enemy enemy)
         {
             Debug.Log($"Enemy missing {enemy.name}");
+            
+            _weaponAI.StopFiring(enemy);
         }
     }   
 }
--- Assets/Code/Weapon/WeaponAI.cs
+++ Assets/Code/Weapon/WeaponAI.cs
@@ -1,9 +1,43 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Code.Weapon
 {
     public class WeaponAI : MonoBehaviour
     {
+        [SerializeField] private Weapon _weapon;
         
+        private Enemy _currentTarget;
+        
+        public void StartFiring(Enemy enemy)
+        {
+            if(Equals(_currentTarget, enemy))
+                return;
+            
+            _currentTarget = enemy;
+        }
+
+        public void StopFiring(Enemy enemy)
+        {
+            if(_currentTarget != enemy)
+                return;
+            
+            _currentTarget = null;
+        }
+        
+        public async UniTask IdleState()
+        {
+            
+        }
+
+        public async UniTask FollowToTargetState()
+        {
+            
+        }
+        
+        public async UniTask ShootTargetState()
+        {
+            
+        }
     }
 }
