2227f2d7a44ba4e2592ceac11d1982a756b43036
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-06-03 13:00:11 +0300

    edit: gizmos target circle detector

--- Assets/Code/VisionCone/Detectors/TargetCircleDetector.cs
+++ Assets/Code/VisionCone/Detectors/TargetCircleDetector.cs
@@ -12,8 +12,8 @@ namespace Code.VisionCone.Detectors
         [SerializeField] private LayerMask _targetMask;
         [SerializeField] private LayerMask _obstacleMask;
         [SerializeField] private Transform _body;
-        [Space(10)]
-        [SerializeField] private bool _isShowDebug = true;
+        [Space(10)] [SerializeField] private bool _isShowDebug = true;
+        [SerializeField] private int _precision = 64;
 
         private Enemy _lastTarget;
 
@@ -64,19 +64,51 @@ namespace Code.VisionCone.Detectors
             return null;
         }
 
+#if UNITY_EDITOR
         private void OnDrawGizmosSelected()
         {
             if (!_isShowDebug || !_body)
                 return;
 
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, _range);
+            Vector3 origin = _body.position;
 
             Vector3 left = Quaternion.Euler(0, -_viewAngle / 2f, 0) * _body.forward;
             Vector3 right = Quaternion.Euler(0, _viewAngle / 2f, 0) * _body.forward;
             Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(_body.position, _body.position + left * _range);
-            Gizmos.DrawLine(_body.position, _body.position + right * _range);
+            Gizmos.DrawLine(origin, origin + left * _range);
+            Gizmos.DrawLine(origin, origin + right * _range);
+
+            float step = _viewAngle / _precision;
+            float startAngle = -_viewAngle / 2f;
+
+            for (float a = 0; a <= _precision; a++)
+            {
+                float angle = startAngle + step * a;
+                Quaternion dirRot = Quaternion.Euler(0, angle, 0);
+                Vector3 dir = dirRot * _body.forward;
+
+                Color color = Color.green;
+
+                if (Physics.Raycast(origin, dir, out RaycastHit hit, _range, _obstacleMask | _targetMask))
+                {
+                    if (hit.collider.TryGetComponent(out Enemy enemyHit))
+                    {
+                        color = Color.yellow;
+                    }
+                    else
+                    {
+                        color = Color.red;
+                    }
+
+                    Gizmos.color = color;
+                    Gizmos.DrawLine(origin, hit.point);
+                }
+                else
+                {
+                    Gizmos.color = color;
+                    Gizmos.DrawLine(origin, origin + dir * _range);
+                }
+            }
 
             Collider[] colliders = Physics.OverlapSphere(transform.position, _range, _targetMask);
             foreach (var col in colliders)
@@ -94,33 +126,13 @@ namespace Code.VisionCone.Detectors
                               distance < _range &&
                               height < _heightAbove && height > -_heightBelow;
 
-                Color color = Color.gray;
-
                 if (inCone)
                 {
-                    Vector3 rayDir = flatDir.normalized;
-                    if (Physics.Raycast(_body.position, rayDir, out RaycastHit hit, distance, _obstacleMask))
-                    {
-                        if (hit.transform == enemy.transform || hit.transform.IsChildOf(enemy.transform))
-                        {
-                            color = Color.green;
-                        }
-                        else
-                        {
-                            color = Color.red;
-                        }
-                    }
-                    else
-                    {
-                        color = Color.green;
-                    }
+                    Gizmos.color = Color.cyan;
+                    Gizmos.DrawWireSphere(enemy.transform.position, 0.3f);
                 }
-
-                Gizmos.color = color;
-                Gizmos.DrawLine(_body.position, enemy.transform.position);
-                Gizmos.DrawWireSphere(enemy.transform.position, 0.2f);
             }
         }
-
+#endif
     }
 }
